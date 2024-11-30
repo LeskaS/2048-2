65845a6cd17b69c18e8b34dc3d1ea912d0a66215
Lesya <lesya413@mail.ru>
2024-11-30 08:37:45 +0300

    ff

---
+++ Assets/Scripts/TileCell.cs
@@ -0,0 +1,15 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TileCell : MonoBehaviour
+{
+   public Vector2Int coordinates {get; set;}
+   public Tile tile {get; set;}
+
+   public bool empty => tile == null;
+    public bool occupied => tile != null;
+    
+}
